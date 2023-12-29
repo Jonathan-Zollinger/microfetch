febdd31a3906cf026325c6baa041af6edd454cc1
jonathan zollinger <jonathan.zollinger@outlook.com>
2023-12-28 21:46:02 -0700

    feature: add initial logos up to windows
    remove constructor without color palette
    remove any enums without a default palette
    
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/test/groovy/com/github/jonathan/zollinger/AsciiEnumSpec.groovy
+++ src/test/groovy/com/github/jonathan/zollinger/AsciiEnumSpec.groovy
@@ -3,12 +3,17 @@ package com.github.jonathan.zollinger
 import com.github.jonathan.zollinger.model.AsciiEnum
 import spock.lang.Specification
 
+import static com.diogonunes.jcolor.Ansi.colorize
+import static com.diogonunes.jcolor.Attribute.TEXT_COLOR
+
 class AsciiEnumSpec extends Specification {
 
     void "print all enums sans error"() {
         when:
         for(AsciiEnum distro: AsciiEnum.getEnumConstants()){
+            System.println(colorize("vvvvvvvvv new ascii image below    vvvvvvvvv", TEXT_COLOR(0)))
             System.println(distro)
+            System.println(colorize("^^^^^^^^^ finish above ascii image ^^^^^^^^^", TEXT_COLOR(0)))
         }
         then:
         noExceptionThrown()
@@ -20,15 +25,13 @@ class AsciiEnumSpec extends Specification {
         System.println(distro)
         System.println("^^^^^^^^^ finish above ascii image ^^^^^^^^^")
 
-
         then:
         noExceptionThrown()
 
         where:
         distro                  | _
-        AsciiEnum.ALMA_LINUX    | _
-        AsciiEnum.ANTERGOS      | _
-
+        AsciiEnum.KALI          | _
+        AsciiEnum.WINDOWS_10    | _
 
     }
 }
