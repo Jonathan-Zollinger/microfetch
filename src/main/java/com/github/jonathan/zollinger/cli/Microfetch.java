d04faa0b514970a6b01450718a31bab7af5f30a3
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-01-02 16:09:28 -0700

    feature: introduce version query support
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/github/jonathan/zollinger/cli/Microfetch.java
+++ src/main/java/com/github/jonathan/zollinger/cli/Microfetch.java
@@ -4,23 +4,34 @@ import com.github.jonathan.zollinger.cli.util.AsciiEnumTypeConverter;
 import com.github.jonathan.zollinger.cli.util.MicrofetchVersionProvider;
 import com.github.jonathan.zollinger.model.AsciiEnum;
 import io.micronaut.configuration.picocli.PicocliRunner;
+import jakarta.inject.Inject;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.util.Arrays;
+
 @Command(name = "microfetch",
-        description = "System info tool written with Java, built with graalvm",
-        versionProvider = MicrofetchVersionProvider.class)
+        description = "System info tool written with Java, built with graalvm")
 public class Microfetch implements Runnable {
+    @Inject
+    MicrofetchVersionProvider versionProvider;
     @Option(names = {"--distro", "--os"},
             description = "OS or linux distro",
             converter = AsciiEnumTypeConverter.class)
     AsciiEnum distro = AsciiEnum.LINUX;
+
+    @Option(names = {"version", "--version", "-v"})
+    boolean version = false;
 
     public static void main(String[] args) {
         PicocliRunner.run(Microfetch.class, args);
     }
 
     public void run() {
-        System.out.println(distro);
+        if (version) {
+            System.out.println(Arrays.toString(versionProvider.getVersion()));
+        }else {
+            System.out.println(distro);
+        }
     }
 }
--- src/test/groovy/com/github/jonathan/zollinger/MicrofetchCliSpec.groovy
+++ src/test/groovy/com/github/jonathan/zollinger/MicrofetchCliSpec.groovy
@@ -4,6 +4,7 @@ import com.github.jonathan.zollinger.cli.Microfetch
 import com.github.jonathan.zollinger.model.AsciiEnum
 import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Value
 import io.micronaut.context.env.Environment
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.AutoCleanup
@@ -25,6 +26,9 @@ class MicrofetchCliSpec extends Specification {
     @Shared @AutoCleanup
     ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
 
+    @Shared @Value('${micronaut.application.version}')
+    String version = "0.0.1"
+
     def setup() {
         outputStream.reset()
         errStream.reset()
@@ -35,6 +39,22 @@ class MicrofetchCliSpec extends Specification {
     def cleanup() {
         System.setOut(originalOut)
         System.setErr(originalErr)
+    }
+
+    def "query version without error"() {
+        when:
+        String[] args = new String[]{versionArg}
+        PicocliRunner.run(Microfetch, ctx, args)
+
+        then:
+        outputStream.toString() == "[Microfetch Version $version]${System.lineSeparator()}"
+        errStream.toString().isEmpty()
+
+        where:
+        versionArg  | _
+        "--version" | _
+        "version"   | _
+        "-v"        | _
     }
 
     @Unroll
