9642e2ac3d667d21dd453dfd74c56996c7e258b8
jonathan zollinger <jonathan.zollinger@outlook.com>
2024-09-08 17:28:36 -0600

    Feat: add distro auto-detection
    Signed-off-by: jonathan zollinger <jonathan.zollinger@outlook.com>

--- src/main/java/com/github/jonathan/zollinger/cli/Microfetch.java
+++ src/main/java/com/github/jonathan/zollinger/cli/Microfetch.java
@@ -1,14 +1,16 @@
 package com.github.jonathan.zollinger.cli;
 
-import com.github.jonathan.zollinger.cli.util.AsciiEnumTypeConverter;
 import com.github.jonathan.zollinger.cli.util.MicrofetchVersionProvider;
 import com.github.jonathan.zollinger.model.AsciiEnum;
 import io.micronaut.configuration.picocli.PicocliRunner;
 import jakarta.inject.Inject;
+import oshi.SystemInfo;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 @Command(name = "microfetch",
         description = "System info tool written with Java, built with graalvm")
@@ -18,7 +20,7 @@ public class Microfetch implements Runnable {
     @Option(names = {"--distro", "--os"},
             description = "OS or linux distro",
             converter = AsciiEnumTypeConverter.class)
-    AsciiEnum distro = AsciiEnum.LINUX;
+    AsciiEnum distro;
 
     @Option(names = {"version", "--version", "-v"})
     boolean version = false;
@@ -30,8 +32,23 @@ public class Microfetch implements Runnable {
     public void run() {
         if (version) {
             System.out.println(Arrays.toString(versionProvider.getVersion()));
-        }else {
-            System.out.println(distro);
+        } else System.out.println(Objects.requireNonNullElseGet(distro, () -> Arrays.stream(AsciiEnum.values())
+                .filter(thistro -> thistro.name().equalsIgnoreCase(
+                        new SystemInfo().getOperatingSystem().getFamily()))
+                .findFirst()
+                .orElse(AsciiEnum.LINUX)));
+    }
+
+    /**
+     * Converts string to ascii enum object. If no match is found, default to linux
+     */
+    static class AsciiEnumTypeConverter implements CommandLine.ITypeConverter<AsciiEnum> {
+        @Override
+        public AsciiEnum convert(String string) {
+            return Arrays.stream(AsciiEnum.values())
+                    .filter(thistro -> thistro.name().equalsIgnoreCase(string))
+                    .findFirst()
+                    .orElse(AsciiEnum.LINUX);
         }
     }
 }
